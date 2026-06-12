8177a0ab4f74d030eda7fad725d739e8aa835026
juvel00 <juvelbinoj123@gmail.com>
2026-06-12 19:36:03 +0530

    added models and lib

---
+++ models/user.ts
@@ -0,0 +1,25 @@
+import bcrypt from "bcryptjs";
+import mongoose, { model } from "mongoose";
+export interface IUser{
+    email:string
+    password:string
+    _id?:mongoose.Types.ObjectId
+    createdAt?:Date
+    updatedAt?:Date
+}
+const userSchema = new mongoose.Schema<IUser>({
+    email:{type:String, required:true, unique:true},
+    password:{type:String, required:true}
+},{
+    timestamps:true
+})
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password=await bcrypt.hash(this.password,10)
+    }
+    
+})
+
+const User = mongoose.models.User || model<IUser>("User", userSchema)
+
+export default User
